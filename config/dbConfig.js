adf01b622fbc8c0d507bb56de6ec2a7f85f9c1cf
Nalipp <nateplipp@gmail.com>
2017-08-23 13:44:47 +0900

    configure mlab db

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -3,7 +3,7 @@ var mongoose = require('mongoose');
 if (process.env.NODE_ENV === 'dev') {
   url = 'mongodb://localhost:27017/nodeToyDev';
 } else {
-  url = 'mongodb://localhost:27017/nodeToy';
+  url = 'mongodb://heroku_dq13zd86:q5eilfck8e418hr07rtk8v2tqj@ds151433.mlab.com:51433/heroku_dq13zd86';
 }
 
 mongoose.connect(url, err => {
